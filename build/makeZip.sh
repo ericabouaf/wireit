a766e07fb4d5586b42da15088a1c3c71fc5f3767
Eric Abouaf <eric.abouaf@gmail.com>
2008-10-12 17:20:41 +0000

    Removing everything in the old repository

--- build/makeZip.sh
+++
@@ -1,5 +0,0 @@
-#
-# This is the command line to create a zip file
-#
-cd ../..
-zip -vr wireit.zip WireIt -x "*.svn*" "*.DS_Store*" "*build.sh" "*makeZip.sh" "*summary.txt" "*logo-wireit.xcf" "*generateDoc.sh" "*Dfly*"
--- doc/generateDoc.sh
+++
@@ -1,4 +0,0 @@
-#
-# Simple script that builds the API documentation using JSDoc
-#
-perl ../../JSDoc-1.10.2/jsdoc.pl -r ../js --project-name WireIt --project-summary summary.txt
