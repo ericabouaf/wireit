d177778f2d403a1312887b6db1ae87a82c052543
Eric Abouaf <eric.abouaf@gmail.com>
2009-05-17 13:57:03 +0200

    Releaseing 0.4.0

--- build/makeZip.sh
+++ build/makeZip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This is the command line to create the wireit distribution zip file
 cd ../..
-zip -vr WireIt-0.4.0.zip WireIt-0.4.0 -x "*.git*" "*.DS_Store*" "*build.sh" "*makeZip.sh" "*/res/*" "*yuidoc.sh" "*/xproc/*" "*/ExhibitMaker/*" "*/jsWireTalk/*" "*/wireitbuilder/*" "*/ExhibitMaker/*" "*/jsWireTalk/*" "*/wireitbuilder/*" "*/doc-parser/*" "*/doc-template/*" "*/PersistingTooltip.js" "*/lib/yui-accordion/*"
+zip -vr WireIt-0.4.0.zip WireIt-0.4.0 -x "*.git*" "*.DS_Store*" "*build.sh" "*makeZip.sh" "*/res/*.xcf" "*/res/presentations/*" "*yuidoc.sh" "*/xproc/*" "*/ExhibitMaker/*" "*/jsWireTalk/*" "*/wireitbuilder/*" "*/ExhibitMaker/*" "*/jsWireTalk/*" "*/wireitbuilder/*" "*/doc-parser/*" "*/doc-template/*" "*/PersistingTooltip.js" "*/lib/yui-accordion/*"
 
