5a4dba03d714262b183f821f147f5e8f41845588
neyric <eric.abouaf@gmail.com>
2012-08-20 17:06:18 +0200

    WireIt 3.0.0pr2

---
+++ api/api.js
@@ -0,0 +1,143 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ArrowWire",
+        "BaseField",
+        "BezierWire",
+        "BidirectionalArrowWire",
+        "Container",
+        "ContainerBase",
+        "Field",
+        "FormContainer",
+        "ImageContainer",
+        "InOutContainer",
+        "Layer",
+        "StraightWire",
+        "Terminal",
+        "TerminalBase",
+        "TerminalDDGroups",
+        "TerminalDragEdit",
+        "TerminalInput",
+        "TerminalOutput",
+        "TerminalScissors",
+        "TextareaContainer",
+        "WidgetIcons",
+        "WireBase",
+        "WiresDelegate"
+    ],
+    "modules": [
+        "arrow-wire",
+        "bezier-wire",
+        "container",
+        "container-base",
+        "form-container",
+        "image-container",
+        "inout-container",
+        "inputex-wirable",
+        "layer",
+        "straight-wire",
+        "terminal",
+        "terminal-base",
+        "terminal-ddgroups",
+        "terminal-dragedit",
+        "terminal-input",
+        "terminal-output",
+        "terminal-scissors",
+        "textarea-container",
+        "widget-icons",
+        "wire-base",
+        "wireit-app",
+        "wires-delegate"
+    ],
+    "allModules": [
+        {
+            "displayName": "arrow-wire",
+            "name": "arrow-wire"
+        },
+        {
+            "displayName": "bezier-wire",
+            "name": "bezier-wire"
+        },
+        {
+            "displayName": "container",
+            "name": "container"
+        },
+        {
+            "displayName": "container-base",
+            "name": "container-base"
+        },
+        {
+            "displayName": "form-container",
+            "name": "form-container"
+        },
+        {
+            "displayName": "image-container",
+            "name": "image-container"
+        },
+        {
+            "displayName": "inout-container",
+            "name": "inout-container"
+        },
+        {
+            "displayName": "inputex-wirable",
+            "name": "inputex-wirable"
+        },
+        {
+            "displayName": "layer",
+            "name": "layer"
+        },
+        {
+            "displayName": "straight-wire",
+            "name": "straight-wire"
+        },
+        {
+            "displayName": "terminal",
+            "name": "terminal"
+        },
+        {
+            "displayName": "terminal-base",
+            "name": "terminal-base"
+        },
+        {
+            "displayName": "terminal-ddgroups",
+            "name": "terminal-ddgroups"
+        },
+        {
+            "displayName": "terminal-dragedit",
+            "name": "terminal-dragedit"
+        },
+        {
+            "displayName": "terminal-input",
+            "name": "terminal-input"
+        },
+        {
+            "displayName": "terminal-output",
+            "name": "terminal-output"
+        },
+        {
+            "displayName": "terminal-scissors",
+            "name": "terminal-scissors"
+        },
+        {
+            "displayName": "textarea-container",
+            "name": "textarea-container"
+        },
+        {
+            "displayName": "widget-icons",
+            "name": "widget-icons"
+        },
+        {
+            "displayName": "wire-base",
+            "name": "wire-base"
+        },
+        {
+            "displayName": "wireit-app",
+            "name": "wireit-app"
+        },
+        {
+            "displayName": "wires-delegate",
+            "name": "wires-delegate"
+        }
+    ]
+} };
+});
