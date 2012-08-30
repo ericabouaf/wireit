75ec00faa2c5161296d03b7d93e0208b69c1328c
neyric <eric.abouaf@gmail.com>
2012-08-30 11:55:27 +0200

    Docs docs

--- build/widget-icons/widget-icons-coverage.js
+++ build/widget-icons/widget-icons-coverage.js
@@ -26,9 +26,9 @@ _yuitest_coverage["build/widget-icons/widget-icons.js"] = {
     path: "build/widget-icons/widget-icons.js",
     code: []
 };
-_yuitest_coverage["build/widget-icons/widget-icons.js"].code=["YUI.add('widget-icons', function (Y, NAME) {","","/**"," * @module widget-icons"," */","","/**"," * @class WidgetIcons"," * @constructor"," * @param {Object} config configuration object"," */","Y.WidgetIcons = function (config) {","","   Y.after(this._renderUIicons, this, \"renderUI\");","   ","};","","Y.WidgetIcons.ATTRS = {","   ","   /**","    * Set of icons","    * @attribute icons","    */","   icons: {","      value: []","   }","   ","};","","Y.WidgetIcons.prototype = {","   ","   _renderUIicons: function () {","      ","      var p = this.get('contentBox'),","          that= this;","      Y.Array.each( this.get('icons'), function (icon) {","         var i = Y.Node.create('<span class=\"'+that.getClassName('icon')+' '+icon.className+'\" title=\"'+icon.title+'\"></span>');","         i.on('click', Y.bind(that[icon.click], that) );","         i.appendTo( p );","      });","      ","   }","   ","};","","","","}, '@VERSION@', {\"requires\": [], \"skinnable\": true});"];
-_yuitest_coverage["build/widget-icons/widget-icons.js"].lines = {"1":0,"12":0,"14":0,"18":0,"30":0,"34":0,"36":0,"37":0,"38":0,"39":0};
-_yuitest_coverage["build/widget-icons/widget-icons.js"].functions = {"WidgetIcons:12":0,"(anonymous 2):36":0,"_renderUIicons:32":0,"(anonymous 1):1":0};
+_yuitest_coverage["build/widget-icons/widget-icons.js"].code=["YUI.add('widget-icons', function (Y, NAME) {","","/**"," * @module widget-icons"," */","","/**"," * @class WidgetIcons"," * @constructor"," * @param {Object} config configuration object"," */","Y.WidgetIcons = function (config) {","","   Y.after(this._renderUIicons, this, \"renderUI\");","   ","};","","Y.WidgetIcons.ATTRS = {","   ","   /**","    * Set of icons","    * @attribute icons","    */","   icons: {","      value: []","   }","   ","};","","Y.WidgetIcons.prototype = {","   ","   _renderUIicons: function () {","      ","      var p = this.get('contentBox'),","          that= this;","          ","      Y.Array.each( this.get('icons'), function (icon) {","         var i = Y.Node.create('<span class=\"'+that.getClassName('icon')+' '+icon.className+'\" title=\"'+icon.title+'\"></span>');","         i.on('click', Y.bind(that[icon.click], that) );","         i.appendTo( p );","         //p.insertBefore(i, p.get('children').item(0) );","      });","      ","   }","   ","};","","","","}, '@VERSION@', {\"requires\": [], \"skinnable\": true});"];
+_yuitest_coverage["build/widget-icons/widget-icons.js"].lines = {"1":0,"12":0,"14":0,"18":0,"30":0,"34":0,"37":0,"38":0,"39":0,"40":0};
+_yuitest_coverage["build/widget-icons/widget-icons.js"].functions = {"WidgetIcons:12":0,"(anonymous 2):37":0,"_renderUIicons:32":0,"(anonymous 1):1":0};
 _yuitest_coverage["build/widget-icons/widget-icons.js"].coveredLines = 10;
 _yuitest_coverage["build/widget-icons/widget-icons.js"].coveredFunctions = 4;
 _yuitest_coverline("build/widget-icons/widget-icons.js", 1);
@@ -75,15 +75,17 @@ Y.WidgetIcons.prototype = {
 _yuitest_coverline("build/widget-icons/widget-icons.js", 34);
 var p = this.get('contentBox'),
           that= this;
-      _yuitest_coverline("build/widget-icons/widget-icons.js", 36);
+          
+      _yuitest_coverline("build/widget-icons/widget-icons.js", 37);
 Y.Array.each( this.get('icons'), function (icon) {
-         _yuitest_coverfunc("build/widget-icons/widget-icons.js", "(anonymous 2)", 36);
-_yuitest_coverline("build/widget-icons/widget-icons.js", 37);
+         _yuitest_coverfunc("build/widget-icons/widget-icons.js", "(anonymous 2)", 37);
+_yuitest_coverline("build/widget-icons/widget-icons.js", 38);
 var i = Y.Node.create('<span class="'+that.getClassName('icon')+' '+icon.className+'" title="'+icon.title+'"></span>');
-         _yuitest_coverline("build/widget-icons/widget-icons.js", 38);
-i.on('click', Y.bind(that[icon.click], that) );
          _yuitest_coverline("build/widget-icons/widget-icons.js", 39);
+i.on('click', Y.bind(that[icon.click], that) );
+         _yuitest_coverline("build/widget-icons/widget-icons.js", 40);
 i.appendTo( p );
+         //p.insertBefore(i, p.get('children').item(0) );
       });
       
    }
--- build/widget-icons/widget-icons.js
+++ build/widget-icons/widget-icons.js
@@ -33,10 +33,12 @@ Y.WidgetIcons.prototype = {
       
       var p = this.get('contentBox'),
           that= this;
+          
       Y.Array.each( this.get('icons'), function (icon) {
          var i = Y.Node.create('<span class="'+that.getClassName('icon')+' '+icon.className+'" title="'+icon.title+'"></span>');
          i.on('click', Y.bind(that[icon.click], that) );
          i.appendTo( p );
+         //p.insertBefore(i, p.get('children').item(0) );
       });
       
    }
