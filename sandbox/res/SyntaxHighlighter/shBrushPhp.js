1c0859b963fed331982d8ab4e9b31f65e36a84a0
Flaviu Tamas <tamasflaviu@gmail.com>
2017-05-29 16:57:01 -0400

    Fix HTTPS

--- sandbox/res/SyntaxHighlighter/shBrushPhp.js
+++ sandbox/res/SyntaxHighlighter/shBrushPhp.js
@@ -25,7 +25,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with SyntaxHighlighter.  If not, see <http://www.gnu.org/copyleft/lesser.html>.
+ * along with SyntaxHighlighter.  If not, see <https://www.gnu.org/copyleft/lesser.html>.
  */
 SyntaxHighlighter.brushes.Php = function()
 {
